d21b2770ccdf6c3368b7c87b8acf5c274a076a43
HD-Nyx <raycastoflight@gmail.com>
2025-12-14 00:58:19 +1300

    V0.15
    I forgot to save Main.lua and had to go into my minecraft saves folder to get the changes...
    
    Added starter weapon, ammo and sword + some minor changes in Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,15 @@
+local StarterGun = "pointblank:m1911a1"
+local StarterAmmo = "pointblank:ammo45acp"
+local StarterAmmoAmount = 28
+local StarterMelee = "minecraft:wooden_sword{Unbreakable:1b}"
+
+commands.exec('/gamemode adventure @a')
+commands.exec('/clear @a')
+commands.exec('/scoreboard players set @a Points 0')
+commands.exec('/give @a ' .. StarterGun)
+commands.exec('/give @a ' .. StarterAmmo .. ' ' .. StarterAmmoAmount)
+commands.exec('/give @a ' .. StarterMelee)
+
 local function SpawnGenericZombie(Amount, Health, Damage, Speed, FollowRange)
     for I = 1, Amount do
         local Command = '/summon zombie ~ ~ ~ {Health:'..Health..',Attributes:[{Name:"generic.max_health",Base:'..Health..'},{Name:"generic.attack_damage",Base:'..Damage..'},{Name:"generic.movement_speed",Base:'..Speed..'},{Name:"generic.follow_range",Base:'..FollowRange..'}]}'
